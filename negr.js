fb0628f935a35d54a2ca60c65c8e881abe426e94
Sub-Zerro <85824405+Sub-Zerro@users.noreply.github.com>
2024-07-18 14:43:56 +0500

    Add files via upload

---
+++ negr.js
@@ -0,0 +1,4 @@
+let td = document.querySelector('body > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td:nth-child(2)');
+let img = document.createElement('img');
+img.src = "https://avatars.mds.yandex.net/i?id=e6261ea07369f2df55d0fd7f63980314c8eb346d0eae38ca-5339410-images-thumbs&n=13";
+td.appendChild(img);
